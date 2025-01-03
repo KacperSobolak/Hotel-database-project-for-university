30022bb5dd20251baaa3ebba628e376b121021a9
KacperSobolak <kacpersobolak03@gmail.com>
2025-01-03 22:04:23 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hotel
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly ServiceProvider _serviceProvider;
+
+        public App()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddSingleton<MainWindow>();
+
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+            base.OnStartup(e);
+        }
+    }
+
+}
---
+++ MVVM/View/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using Microsoft.Data.SqlClient;
+using Npgsql;
+
+namespace Hotel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string connString = "Host=sequentially-in-mackerel.data-1.use1.tembo.io;Port=5432;Username=postgres;Password=Be2gkgU6omPjbX3P;Database=postgres";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+    }
+}
