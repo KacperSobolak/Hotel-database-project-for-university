da09aa9c9634dac189614d82132f1879a986a8b6
KacperSobolak <kacpersobolak03@gmail.com>
2025-01-06 11:20:30 +0100

    guest picker

---
+++ MVVM/Viewmodel/PopUp/GuestPickerViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Hotel.MVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+using Hotel.Core;
+using Hotel.Repositories;
+
+namespace Hotel.MVVM.Viewmodel.PopUp
+{
+    internal class GuestPickerViewModel : ViewModel
+    {
+        private readonly IGuestsRepository _guestRepository;
+
+        public ObservableCollection<Guest> Guests { get; set; }
+        public Guest SelectedGuest { get; set; }
+
+        public RelayCommand PickGuestCommand { get; }
+
+        public GuestPickerViewModel(IGuestsRepository guestRepository)
+        {
+            _guestRepository = guestRepository;
+            Guests = new ObservableCollection<Guest>(_guestRepository.GetGuests());
+
+            PickGuestCommand = new RelayCommand(PickGuest, o => true);
+        }
+
+        private void PickGuest(object parameter)
+        {
+            var guest = parameter as Guest;
+            SelectedGuest = guest;
+            MessageBox.Show($"Guest {guest.Name} {guest.Surname} has been picked.", "Guest Picked", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
